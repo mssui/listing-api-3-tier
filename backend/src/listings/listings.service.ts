e48ba8128ea071141f0348830d5302d9b919e9c6
Asli Ari <asliari2012@gmail.com>
2025-10-22 12:27:04 +0200

    Add: New getAll endpoint

--- backend/src/listings/listings.service.ts
+++ backend/src/listings/listings.service.ts
@@ -8,7 +8,7 @@ import { ListingsRepository } from './listings.repository';
 
 @Injectable()
 export class ListingsService {
-    constructor(private readonly listingRepository: ListingsRepository) {}
+    constructor(private readonly listingRepository: ListingsRepository) { }
 
     async create(newList: CreateListingDto): Promise<any> {
         console.log('Create hits', newList);
@@ -20,7 +20,7 @@ export class ListingsService {
         return this.listingRepository.createListing(newList);
     }
 
-     async update(newList: UpdateListingDto): Promise<any> {
+    async update(newList: UpdateListingDto): Promise<any> {
         console.log('Create hits', newList);
         // const newListing: CreateListingDto = new CreateListingDto(...newList);
 
@@ -30,7 +30,7 @@ export class ListingsService {
         return this.listingRepository.updateListing(newList);
     }
 
-     async delete(listingId: DeleteListingDto): Promise<any> {
+    async delete(listingId: DeleteListingDto): Promise<any> {
         console.log('Delete hits', listingId);
         // const newListing: CreateListingDto = new CreateListingDto(...newList);
 
@@ -38,5 +38,15 @@ export class ListingsService {
         // newListing.onHold = false;
 
         return this.listingRepository.deleteListingById(listingId);
+    }
+
+    async findAll(): Promise<any> {
+
+        // const newListing: CreateListingDto = new CreateListingDto(...newList);
+
+        // console.log('Create generates', newListing)
+        // newListing.onHold = false;
+
+        return this.listingRepository.findAll();
     }
 }
